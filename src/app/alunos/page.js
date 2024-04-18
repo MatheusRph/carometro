9138973cba1017aea052930f17734f846c3b7e82
enzo151 <enzobussi3103@gmail.com>
2024-04-18 17:27:42 -0300

    mergeCadastro2

---
+++ src/app/alunos/page.js
@@ -0,0 +1,11 @@
+import NameAluno from "@/components/nameAluno";
+import MyNavbar from "@/components/nav-bar";
+
+export default function Alunos() {
+  return (
+    <>
+      <MyNavbar></MyNavbar>
+      <NameAluno></NameAluno>
+    </>
+  );
+}
