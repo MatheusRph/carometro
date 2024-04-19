7c65d804ad9c331a22a12fc58ee6a3ab5cc96938
MatheusRph <matheus.rodolpho16@gmail.com>
2024-04-19 10:44:31 -0300

    yourpastes

--- src/components/nav-bar.js
+++ src/components/nav-bar.js
@@ -1,19 +1,20 @@
 // pages/index.js
 import Image from "next/image";
+import "./styles/nav-bar.css"
 
 export default function MyNavbar() {
     return (
         <>
             <nav className="navbar navbar-expand-lg">
-                <div className="container-fluid">
-                    <a href="/">
+                <div className="container-fluid ">
+                    <a href="/" className="navbar-brand">
                         <Image src="https://www.sp.senai.br/images/senai.svg" alt="a" width={150} height={50} className="nav-brand" />
                     </a>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <button className="navbar-toggler justify-content-end" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div className="collapse navbar-collapse justify-content-center" id="navbarNav">
-                        <ul className="navbar-nav">
+                    <div className="collapse navbar-collapse text-center " id="navbarNav">
+                        <ul className="navbar-nav  text-center">
                             <li className="nav-item">
                                 <a className="nav-link active" href="/">Home</a>
                             </li>
---
+++ src/components/yourpaste.js
@@ -0,0 +1,21 @@
+import './styles/yourpaste.css'
+
+export default function Yourpastes() {
+    return (
+        <>
+            <div class="container text-center yourpastes">
+                <div class="col align-items-start">
+                    <div class="col-12 bg-primary">
+                        <input className='col-8 jomhuria-regular text-black' placeholder='Olá galera'></input>
+                    </div>
+                    <div class="col">
+                        One of three columns
+                    </div>
+                    <div class="col">
+                        One of three columns
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
