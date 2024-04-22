b57ed8ebd9e24cd1e2a3dd6ca69ce5c913449581
MatheusRph <matheus.rodolpho16@gmail.com>
2024-04-22 16:16:11 -0300

    MatheusRph Brach

--- src/components/yourpaste.js
+++ src/components/yourpaste.js
@@ -1,40 +1,56 @@
- import './styles/yourpaste.css'
- import Image from 'next/image'
+import './styles/yourpaste.css';
+import Image from 'next/image';
 
 export default function Yourpastes() {
     return (
-        <>
-            <div class="container col-8 text-center yourpastes d-flex">
-                <div class="col-12 align-items-start d-flex flex-column">
-                    <div class="col-12 div-input d-flex align-items-center justify-content-center">
-                        <div className='input-wrapper d-flex align-items-center justify-content-center'>
-                            <Image src="/image/search.png" alt="a" class="search-img" width={30} height={30} className="nav-brand" />
-                            <input className='col-11 jomhuria-regular' placeholder='Search...'></input>
-                        </div>
-                    </div>
-                    <div class="col div-class col-12 d-flex align-items-center justify-content-center flex-row">
-                        <div className='class col-3'>
-                            a
-                        </div>
-                        
-                        <div className='class col-3'>
-                            a
-                        </div>
-                        
-                        <div className='class col-3'>
-                            a
-                        </div>
-                        <div className='class col-3'>
-                            a
-                        </div>
-                        
-                        <div className='class col-3'>
-                            a
-                        </div>
-                        
+        <div className="container col-8 text-center yourpastes d-flex">
+            <div className="col-12 align-items-start d-flex flex-column">
+                <div className="col-12 div-input d-flex align-items-center justify-content-center">
+                    <div className='input-wrapper'>
+                        <Image src="/image/search.png" alt="a" className="search-img" width={30} height={30} className="nav-brand" />
+                        <input className='col-11 jomhuria-regular' placeholder='Search...' />
                     </div>
                 </div>
+                <div className="col div-class col-12 d-flex flex-wrap justify-content-center">
+                    <div className='class idev'>
+                        <h2>Idev-1</h2>
+                    </div>
+
+                    <div className='class idev'>
+                        <h2>Idev-2</h2>
+                    </div>
+
+                    <div className='class idev'>
+                        <h2>Idev-3</h2>
+                    </div>
+
+                    <div className='class elec-mec'>
+                        <h2>Elec/Mec-1</h2>
+                    </div>
+
+                    <div className='class elec-mec'>
+                        <h2>Elec/Mec-2</h2>
+                    </div>
+
+                    <div className='class elec-mec'>
+                        <h2>Elec/Mec-3</h2>
+                    </div>
+
+                    <div className='class elec-mec'>
+                        <h2>Elec/Mec-3</h2>
+                    </div>
+
+                    <div className='class elec-mec'>
+                        <h2>Elec/Mec-3</h2>
+                    </div>
+
+                    <div className='class elec-mec'>
+                        <h2>Elec/Mec-3</h2>
+                    </div>
+                    
+
+                </div>
             </div>
-        </>
-    )
+        </div>
+    );
 }
