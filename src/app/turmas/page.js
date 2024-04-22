2f2bf567590f23dc073af814dc96642d398b7757
enzo151 <enzobussi3103@gmail.com>
2024-04-22 17:20:24 -0300

    turmasenzo

---
+++ src/app/turmas/page.js
@@ -0,0 +1,12 @@
+import MyNavbar from "@/components/nav-bar";
+import "./turmas.css"
+import Turma from "@/components/turma";
+
+export default function Turmas() {
+  return (
+    <>
+      <MyNavbar></MyNavbar>
+        <Turma></Turma>
+    </>
+  );
+}
---
+++ src/components/turma.js
@@ -0,0 +1,17 @@
+import './styles/turma.css'
+
+export default function Turma() {
+  return (
+    <>
+      <div class="container start-text">
+        <div class="col d-flex flex-column">
+          <div class="col pt-4 m-4 p-4">1</div>
+          <div class="col pt-4 m-4 p-4">2</div>
+          <div class="col pt-4 m-4 p-4">3</div>
+          <div class="col pt-4 m-4 p-4">4</div>
+          <div class="col pt-4 m-4 p-4">5</div>
+        </div>
+      </div>
+    </>
+  );
+}
